367cbda995328fa75788ed9ccf507050e013b02c
Amy Clase <amyelouisell@gmail.com>
2026-01-08 18:07:17 +0100

    Crear menu y poner layers

---
+++ Proyecto_02/Assets/Scripts/MenuController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void LoadGameScene() 
+    {
+
+        SceneManager.LoadScene("Game");
+        
+    }
+}
